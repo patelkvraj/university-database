f62383826d2dd8e568b8570f42b3245415cc0cfe
Vraj Patel <mycsstuff@outlook.com>
2025-03-03 16:41:26 -0500

    Add "connection" content to config.php file.

--- config.php
+++ config.php
@@ -1,1 +1,18 @@
+<?php
+$servername = "localhost"
+$username = "root";
+$password = "";
+$dbname = "DB2";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Close connection
+mysqli_close($conn);
+?>
 
