dce3ddfd00bbf1cc4b063c821dd0f3610e36686f
Christian Blake <christianblake3333@gmail.com>
2025-03-07 16:46:02 -0500

    added links to student.php for login and account creation

--- config.php
+++ config.php
@@ -1,10 +1,13 @@
 <?php
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "DB2";
+
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+
 if (!$conn) {
-die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
